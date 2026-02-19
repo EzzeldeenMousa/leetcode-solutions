26f4117fd57a4b19373f59ae05705f209d79ef5e
EzzeldeenMousa <ezzeldeenmousa9@gmail.com>
2026-02-19 20:18:50 +0200

    Time: 6 ms (12.97%), Space: 12.6 MB (21.04%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minimumOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        op = 0
+
+        while any(n > 0 for n in nums):
+            x = min(n for n in nums if n > 0)
+
+            nums = [n - x if n > 0 else 0 for n in nums]
+
+            op += 1
+        return op 
+
+            
