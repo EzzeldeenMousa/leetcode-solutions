b605f4e2cd493774e5773538f388fcc0486c0259
EzzeldeenMousa <ezzeldeenmousa9@gmail.com>
2026-02-26 01:02:46 +0200

    Time: 0 ms (100%), Space: 13.5 MB (5.9%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -5,12 +5,16 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        i = 0
+        left = 0
+        right = len(nums) - 1
 
-        for i in range(len(nums)):
-            if target not in nums:
-                return -1
-            elif nums[i] != target:
-                i += 1
+        while left <= right:
+            mid = (left + right) / 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                return i
+                right = mid - 1
+        return -1
